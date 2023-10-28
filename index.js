3ffd514fea80375ca0182feb72dd5ec2d3f5800e
servet-isikli <servetisikli@yahoo.com>
2023-10-28 15:21:29 +0200

    edit

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ const mongoose = require("mongoose"); // Mongoose paketini içe aktar
 
 const port = process.env.PORT || 5000;
 
-
-// Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
-
-
-
-
 // Mongoose Bağlantısı
 mongoose
   .connect(process.env.MONGODB_URI, {
